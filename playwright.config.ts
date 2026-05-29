b5a918b80adfbea9829850551278b7309af5d4a4
Javier Villa Guardado <javier@aidaddy.com>
2026-05-29 09:57:57 -0700

    Run browser tests against app database path

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
     trace: "on-first-retry"
   },
   webServer: {
-    command: "DEMO_MODE=true npm run start",
+    command: "npm run start",
     url: "http://127.0.0.1:3000",
     reuseExistingServer: !process.env.CI,
     timeout: 120_000
